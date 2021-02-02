f34d5d15f78a14256b2d688e5a01254abfff6da9
WestleyK <westleyk@nym.hush.com>
2021-02-01 18:17:10 -0800

    Fixed/added bool.h

--- src/bool.h
+++ src/bool.h
@@ -2,10 +2,10 @@
 //  bool.h
 //  gpack - https://github.com/WestleyR/gpack
 //
-// Created by WestleyR on Jun 16, 2019
+// Created by WestleyR on Feb 1, 2021
 // Source code: https://github.com/WestleyR/gpack
 //
-// Copyright (c) 2019-2021 WestleyR. All rights reserved.
+// Copyright (c) 2021 WestleyR. All rights reserved.
 // This software is licensed under a BSD 3-Clause Clear License.
 // Consult the LICENSE file that came with this software regarding
 // your rights to distribute this software.
